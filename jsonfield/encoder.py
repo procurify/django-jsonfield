0aa50f0abe9e331ce5106d321904d59790961f86
Markus Balaski <mabalaski@gmail.com>
2024-06-26 12:27:47 -0700

    Update library for compatibility with Django 4.2
    - Removed deprecated usages of `force_text` and `ugettext_lazy` from
    the code so that the library will be compatible with Django 4.2.

--- jsonfield/encoder.py
+++ jsonfield/encoder.py
@@ -1,6 +1,6 @@
 from django.db.models.query import QuerySet
 from django.utils import timezone
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.utils.functional import Promise
 import datetime
 import decimal
@@ -20,7 +20,7 @@ class JSONEncoder(json.JSONEncoder):
         # For Date Time string spec, see ECMA 262
         # http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.15
         if isinstance(obj, Promise):
-            return force_text(obj)
+            return force_str(obj)
         elif isinstance(obj, datetime.datetime):
             representation = obj.isoformat()
             if obj.microsecond:
